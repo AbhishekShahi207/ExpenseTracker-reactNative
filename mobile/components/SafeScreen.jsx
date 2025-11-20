2d5d163bd68e2960db340a144ffbe859a62bdeb4
Abhishek Shahi <shahiabhi9900@gmail.com>
2025-11-20 09:42:00 +0530

    auth page completed

--- mobile/components/SafeScreen.jsx
+++ mobile/components/SafeScreen.jsx
@@ -1,16 +1,16 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import {COLORS} from "@/constants/colors"
+import { View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
-const SafeScreen = ({children}) => {
+import { COLORS } from "@/constants/colors";
 
-    const insets =useSafeAreaInsets()
+const SafeScreen = ({ children }) => {
+  const insets = useSafeAreaInsets();
+
   return (
-    <View style={{paddingTop:insets.top, flex:1,backdropColor:COLORS.background}}>
+    <View style={{ paddingTop: insets.top, flex: 1, backgroundColor: COLORS.background }}>
       {children}
     </View>
-  )
-}
+  );
+};
 
-export default SafeScreen
+export default SafeScreen;
